b2053bb1ff0e4726df27ee065f1d6cecf577b5c4
尼维森莫·纳默帝敖 <icocoabeans@gmail.com>
2022-03-02 18:40:50 +0800

    dylib rename

--- src/lib.lua
+++ src/lib.lua
@@ -1,9 +1,9 @@
 lib = {}
 
-local wsm4l = "../lib/wsm4l.so"
+local wsm4l = "../lib/libwsm4l.so"
 
 package.cpath = package.cpath .. ";" .. wsm4l
 
-lib.wsm = require("wsm4l")
+lib.wsm = require("libwsm4l")
 
 return lib
